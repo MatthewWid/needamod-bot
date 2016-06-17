c4dcccb6816a4bbee5e0df8519c4305d7ae538c4
MatthewMob <game1000@hotmail.co.uk>
2016-06-17 22:51:36 +1000

    Fixed bug that causes bot to crash when a non-existent subreddit is linked

--- automobbot.py
+++ automobbot.py
@@ -29,20 +29,24 @@ LOOP_DELAY = 900
 # Amount of posts to get from /new
 GET_POSTS = 5
 
-UA = "/r/NeedAMod Automate Commenter (Update 18) by /u/MatthewMob"
+UA = "/r/NeedAMod Automate Commenter (Update 19) by /u/MatthewMob"
 r = praw.Reddit(UA)
 r.login(USERNAME, PASSWORD, disable_warning=True)
 
 def commentSub(sub, post):
-    m = r.get_subreddit(sub, fetch=True)
-    d1 = datetime.datetime.utcfromtimestamp(m.created_utc)
-    com = "Subreddit Info (/r/" + m.display_name + "):\n\n**Age**: " + str((datetime.datetime.now() - d1).days) + " days\n\n**Subscribers**: " + str(m.subscribers) + "\n\n**Current Mods**: " + str(len(m.get_moderators())) + "\n\n**Over 18**: " + str(m.over18) + CREDIT
-    
-    print("\nCommenting Sub Info")
-    print("Commenting on: " + post.id)
-    print("Comment: " + com + "\n")
+    try:
+        m = r.get_subreddit(sub, fetch=True)
 
-    post.add_comment(com)
+        d1 = datetime.datetime.utcfromtimestamp(m.created_utc)
+        com = "Subreddit Info (/r/" + m.display_name + "):\n\n**Age**: " + str((datetime.datetime.now() - d1).days) + " days\n\n**Subscribers**: " + str(m.subscribers) + "\n\n**Current Mods**: " + str(len(m.get_moderators())) + "\n\n**Over 18**: " + str(m.over18) + CREDIT
+        
+        print("\nCommenting Sub Info")
+        print("Commenting on: " + post.id)
+        print("Comment: " + com + "\n")
+
+        post.add_comment(com)
+    except:
+        print("Non-existent subreddit: " + sub)
 
 def commentOffer(post):
     com = "Here are 3 questions to help people who want to recruit you know what you're like:\n\n1. **How Active are you (Eg, hours per day) and what timezone are you in?**\n\n2. **If you see a highly upvoted post, but it doesn't follow the rules, what would you do?**\n\n3. **In your opinion, what is the most important quality a mod can have?**" + CREDIT
@@ -66,12 +70,9 @@ while True:
                 if submission.is_self == True and submission.selftext:
                     soup = bs4.BeautifulSoup(submission.selftext_html, "lxml")
                     a = soup.find_all("a", href=True)
-                    print(a)
                     if a and len(a) > 0:
                         href = a[0]["href"] + "/"
-                        print(href)
                         getsub = findSub(href)
-                        print(getsub)
                         if getsub != None and len(getsub) > 0:
                             commentSub(getsub[0], submission)
                     else:
