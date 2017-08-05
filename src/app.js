54032d353050f15f675636b70f366072082eb485
MatthewMob <game1000@hotmail.co.uk>
2017-08-05 16:23:51 +1000

    Offer to mod replying and new report option - Added feature where the bot will check if the user is a moderator of the subreddit they are requesting for, and if not, reports the posts. - Added feature where the bot will now reply to 'Offer to mod' posts like the old bot did. - Added config options for marking posts as checked and 'Offer to mod' reply text. - Updated package.json 'Engine' value to latest NodeJS version.

--- src/app.js
+++ src/app.js
@@ -36,9 +36,11 @@ function logPost(postId, text) {
 	console.log("---\n" + postId + "\n" + text + "\n---\n");
 }
 function addToChecked(postId) {
-	checked.push(postId);
-	// Write to file
-	fs.writeFile(config_bot.checkedFile, JSON.stringify(checked));
+	if (config_bot.checking) {
+		checked.push(postId);
+		// Write to file
+		fs.writeFile(config_bot.checkedFile, JSON.stringify(checked));
+	}
 }
 String.prototype.capitalise = function() {
 	return this.charAt(0).toUpperCase() + this.slice(1);
@@ -64,17 +66,16 @@ function update() {
 			(function() {
 				var post_id = slice.children[i].data.name;
 				var msg = "";
-				var reAll = /\/?[rR]\/[a-zA-Z?_\d]+/g;
-				var reRemove = /\/?[rR]\//g;
-				var allText = reAll.exec(slice.children[i].data.title);
+				var reAll_sub = /\/?[rR]\/[a-zA-Z?_\d]+/g;
+				var allText_sub = reAll_sub.exec(slice.children[i].data.title);
 				var canReport = false;
 				var reportReason = config_bot.reports.prefix;
 
-				if (slice.children[i].data.link_flair_text == config_bot.flair_names.css) {
-					msg += config_bot.cssmods_text;
-				}
-				if (allText) {
-					var subName = allText[0].replace(reRemove, "");
+				if (allText_sub) {
+					if (slice.children[i].data.link_flair_text == config_bot.flair_names.css) {
+						msg += config_bot.cssmods_text;
+					}
+					var subName = allText_sub[0].replace(/\/?[rR]\//g, "");
 					
 					reddit("/r/" + subName + "/about").get().then(function(result) {
 						var age = Math.floor(((Date.now() / 1000) - result.data.created) / 60 / 60 / 24);
@@ -82,6 +83,7 @@ function update() {
 						var currentMods = 0;
 						var minimumPosts = false;
 						var nsfw = result.data.over18;
+						var isMod = false;
 
 						reddit("/r/" + subName + "/new").listing().then(function(slice) {
 							if (slice.children.length >= config_bot.minimum_posts) {
@@ -91,13 +93,18 @@ function update() {
 							return reddit("/r/" + subName + "/about/moderators").get();
 						}).then(function(modlist) { // Construct comment message
 							currentMods = modlist.data.children.length;
+							//console.log(modlist.data.children);
+							//console.log(slice.children[i].data.author);
+							if ((modlist.data.children.findIndex(function(e) {return e.name == slice.children[i].data.author;})) != -1) {
+								isMod = true;
+							}
 
 							msg += "Subreddit Info (/r/" + result.data.display_name + "):\n\n";
 							msg += "**Age**: " + decimalsInNum(age) + " days\n\n";
 							msg += "**Subscribers**: " + decimalsInNum(subscribers) + "\n\n";
 							msg += "**Current Mods**: " + decimalsInNum(currentMods) + "\n\n";
 							msg += "**At Least 25 Posts**: " + minimumPosts.toString().capitalise() + "\n\n";
-							msg += "**Over 18**: " + nsfw.toString().capitalise() + "\n\n---\n\n";
+							msg += "**Over 18**: " + nsfw.toString().capitalise();
 							msg += config_bot.credit;
 
 							return reddit("/r/" + subName + "/new").listing();
@@ -111,12 +118,13 @@ function update() {
 									addToChecked(post_id);
 								});
 							} else {
+								console.log(msg);
 								logPost(post_id, "Commenting: " + result.data.display_name);
 								addToChecked(post_id);
 							}
 
 							// Report here
-							if (config_bot.reports.should_report && config_bot.interact) {
+							if (config_bot.reports.should_report) {
 								if (subscribers < config_bot.minimum_subs) {
 									reportReason += config_bot.reports.reason_subs;
 									canReport = true;
@@ -125,14 +133,22 @@ function update() {
 									reportReason += config_bot.reports.reason_posts;
 									canReport = true;
 								}
+								if (!isMod) {
+									reportReason += config_bot.reports.reason_mod;
+									canReport = true;
+								}
 
 								if (canReport) {
-									reddit("/api/report").post({
-										reason: reportReason,
-										thing_id: post_id
-									}).then(function() {
+									if (config_bot.interact) {
+										reddit("/api/report").post({
+											reason: reportReason,
+											thing_id: post_id
+										}).then(function() {
+											logPost(post_id, "Reporting for: " + reportReason);
+										});
+									} else {
 										logPost(post_id, "Reporting for: " + reportReason);
-									});
+									}
 								}
 							}
 						});
@@ -142,8 +158,26 @@ function update() {
 						return;
 					});
 				} else {
-					logPost(post_id, "No subreddit in title: " + slice.children[i].data.title);
-					addToChecked(post_id);
+					var reAll_user = /\/?[uU]\/[a-zA-Z?_\d]+/g;
+					var allText_user = reAll_user.exec(slice.children[i].data.title);
+					var userName = allText_user[0].replace(/\/?[uU]\//g, "");
+					if (allText_user) {
+						msg += config_bot.offermod_text;
+						msg += config_bot.credit;
+						if (config_bot.interact) {
+							reddit("/api/comment").post({
+								text: msg,
+								thing_id: post_id
+							}).then(function() {
+								logPost(post_id, "Commenting: " + allText_user[0]);
+								addToChecked(post_id);
+							});
+						} else {
+							console.log(msg);
+							logPost(post_id, "Commenting: " + allText_user[0]);
+							addToChecked(post_id);
+						}
+					}
 				}
 			})();
 		}
@@ -152,4 +186,4 @@ function update() {
 	});
 }
 update();
-var loop = setInterval(update, config_bot.loop_delay * 1000 * 60 );
+var loop = setInterval(update, config_bot.loop_delay * 1000 * 60);
