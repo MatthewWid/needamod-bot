7ff10fd55b3b6a5832167a92d15a147589e9d5e5
MatthewMob <game1000@hotmail.co.uk>
2018-03-30 02:19:28 +1100

    Began migration to new API wrapper Initial commit The old API wrapper (Snoocore) has become deprecated and the bot is being rewritten from the ground up using Snoowrap.

--- src/app.js
+++ src/app.js
@@ -1,279 +1,132 @@
-var Snoocore = require("snoocore");
+var snoowrap = require("snoowrap");
 var fs = require("fs");
-var path = require("path");
 var mongo = require("mongodb").MongoClient;
-// MongoDB database should have a collection called "posts"
-var assert = require("assert");
-/*
-	Example config_oauth.json:
 
-	{
-		type: "script",
-		key: "",
-		secret: "",
-		username: "",
-		password: "",
-		scope: [
-			"read", "submit", "report"
-		]
-	}
-*/
-/*
-	Example config_db.json:
-
-	{
-		url: "<MongoDB URL>"
-	}
-*/
-
-var config_oauth = JSON.parse(fs.readFileSync(__dirname + "/config_oauth.json"));
-var config_bot = JSON.parse(fs.readFileSync(__dirname + "/config_bot.json"));
-var config_db = JSON.parse(fs.readFileSync(__dirname + "/config_db.json"));
+const config_oauth = JSON.parse(fs.readFileSync(__dirname + "/config_oauth.json"));
+const config_bot = JSON.parse(fs.readFileSync(__dirname + "/config_bot.json"));
+const config_db = JSON.parse(fs.readFileSync(__dirname + "/config_db.json"));
 config_bot.get_posts = Math.min(config_bot.get_posts, 25);
 
-/*
-	MongoDB Entry template:
+const r = new snoowrap(config_oauth);
 
-	{
-		post_id: String
-		author: String,
-		type: String,
-		time_checked: Integer (UTC DateTime)
-		subreddit: {
-			subName: String,
-			age: Integer (UTC DateTime),
-			currentMods: Integer,
-			minimumPosts: Boolean,
-			nsfw: Boolean,
-			isMod: Boolean
-		},
-		user: {}
+var allPosts = [];
+
+// Check if there is an error. If so, print it and exit program
+function checkErrorBlank(err) {
+	if (err !== null) {
+		console.log(err);
+		process.exit();
 	}
-*/
-var posts = [];
+}
 
+// Format and then log a given message
+function log(msg) {
+	console.log("\n", msg);
+}
+
+// Connect to MongoDB, collect all previously checked posts, and run the main function
 mongo.connect(config_db.url, function(err, datab) {
-	assert.equal(err, null);
-	console.log("Successfully connected to MongoDB server.");
+	checkErrorBlank(err);
+	log("Successfully connected to MongoDB server.");
 
-	var col = datab.collection(config_db.col);
-
+	var col = datab.db("needamod-subreddits").collection(config_db.col);
 	col.find({}).toArray(function(err, docs) {
-		assert.equal(err, null);
+		checkErrorBlank(err);
 
-		posts = docs;
+		allPosts = docs;
 		main(datab);
 	});
 });
 
-
 function main(db) {
-	var reddit = new Snoocore({
-		userAgent: "NeedAMod Subreddit Info Commenter (Update 31) by /u/MatthewMob",
-		oauth: config_oauth
-	});
-
-	// \/?[rR]\/
-
-	function decimalsInNum(num) {
+	// Take an integer and return a string that is formatted with decimals. Eg, 3985721 turns into "3,985,721"
+	function formatNum(num) {
 		return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 	}
-	function logPost(postId, text) {
-		console.log("---\n" + postId + "\n" + text + "\n---\n");
-	}
+	// Add a post to the checked list and write it to the MongoDB
 	function addToChecked(data) {
 		if (config_bot.checking) {
-			posts.push(data);
-			// Write to MongoDB
+			allPosts.push(data);
 			db.collection(config_db.col).insertOne(data, function(err, result) {
-				assert.equal(err, null);
-
-				console.log("Successfully written to MongoDB.");
-			});
+				checkErrorBlank(err);
+				log("Successfully written to database.");
+			})
 		}
 	}
+	// Capitalise the string
 	String.prototype.capitalise = function() {
 		return this.charAt(0).toUpperCase() + this.slice(1);
-	}
+	};
 
+	// Main function of the bot that is called each loop
 	function update() {
-		console.log("Checks started\n");
-		reddit("/r/" + config_bot.subreddit + "/new").listing({limit: config_bot.get_posts}).then(function(slice_overall) {
-			for (var i = 0; i < config_bot.get_posts; i++) {
-				if (posts.find(function(e) {return e.post_id == slice_overall.children[i].data.name})) { // Check if post has already been checked
-					/*
-						BUG:
-						When bot starts all posts that are checked are skipped, but then the next three posts after them are also checked (Not intended)
-					*/
-					//logPost(slice_overall.children[i].data.name, "Post already checked: " + slice_overall.children[i].data.name);
+		log("Checks started");
+
+		r.getSubreddit(config_bot.subreddit).getNew({limit: config_bot.get_posts}).then((posts) => {
+			// Quickly filter and remove posts that cannot be checked, then return a list of posts ready to check
+			var validPosts = [];
+			var timeNow = Math.floor(Date.now() / 1000);
+			for (var i = 0; i < posts.length; i++) {
+				// If the post has already been checked do not check it
+				if (allPosts.find((e) => e.post_id == posts[i].id)) {
+					log(posts[i].id + " | Skipping | Already checked.");
 					continue;
 				}
-				var timeDif = (Math.floor((Date.now() / 1000) - slice_overall.children[i].data.created_utc)) / 60;
-				if (timeDif < config_bot.wait_time) { // Check if post is old enough to evaluate
-					logPost(slice_overall.children[i].data.name, "Post too new to evaluate.")
+
+				// If the post is too young to be checked do not check it
+				if ((timeNow - posts[i].created_utc) / 60 < config_bot.wait_time) {
+					log(posts[i].id + " | Skipping | Too young.")
 					continue;
 				}
-				if (slice_overall.children[i].data.link_flair_text == config_bot.flair_names.mod) { // Check if post is a mod post
-					logPost(slice_overall.children[i].data.name, "Skipping mod post");
+
+				// If the post has a "Mod Post" flair do not check it
+				if (posts[i].link_flair_text == config_bot.flair_names.mod) {
+					log(posts[i].id + " | Skipping | Mod post.");
 					continue;
 				}
+
+				validPosts.push(posts[i]);
+			}
+			return validPosts;
+		}).then((posts) => {
+			// Push all promises to subreddit into an array, wait for all promises to be processed in that array
+			var promises = [];
+			for (var l = 0; l < posts.length; l++) {
 				(function() {
-					var post = slice_overall.children[i];
-					var post_id = post.data.name;
+					const i = l;
+					var allText_sub = /\/?[rR]\/[a-zA-Z?_\d]+/g.exec(posts[i].title);
+					var allText_user = /\/?[uU]\/[a-zA-Z?_\d]+/g.exec(posts[i].title);
 					var msg = "";
-					var reAll_sub = /\/?[rR]\/[a-zA-Z?_\d]+/g;
-					var allText_sub = reAll_sub.exec(post.data.title);
 					var canReport = false;
 					var reportReason = config_bot.reports.prefix;
 
-					if (allText_sub) { // If it is a subreddit
-						if (post.data.link_flair_text == config_bot.flair_names.css) {
+					if (allText_sub) { // If the title has a subreddit
+						// If the post has a "css mods needed" flair, prepend the comment with the CSS questions
+						if (posts[i].link_flair_text == config_bot.flair_names.css) {
 							msg += config_bot.cssmods_text;
 						}
+
 						var subName = allText_sub[0].replace(/\/?[rR]\//g, "");
-						
-						reddit("/r/" + subName + "/about").get().then(function(result) {
-							var age = Math.floor(((Date.now() / 1000) - result.data.created) / 60 / 60 / 24);
-							var subscribers = result.data.subscribers;
-							var currentMods = 0;
-							var minimumPosts = false;
-							var nsfw = result.data.over18;
-							var isMod = false;
-
-							reddit("/r/" + subName + "/new").listing({limit: config_bot.minimum_posts + 2}).then(function(slice_new) {
-								if (slice_new.children.length >= config_bot.minimum_posts) {
-									minimumPosts = true;
-								}
-
-								/*
-									BUG:
-									Bot only checks the first 25 moderators, if the author is not in that it will mark them as not being mod.
-								*/
-								return reddit("/r/" + subName + "/about/moderators").get({limit: 100});
-							}).then(function(modlist) { // Construct comment message
-								currentMods = modlist.data.children.length;
-								//console.log(modlist.data.children);
-								//console.log(post.data.author);
-								if ((modlist.data.children.findIndex(function(e) {return e.name == post.data.author;})) != -1) {
-									isMod = true;
-								}
-
-								console.log(age);
-								console.log(subscribers);
-								console.log(currentMods);
-								msg += "Subreddit Info (/r/" + result.data.display_name + "):\n\n";
-								msg += "**Age**: " + decimalsInNum(age) + " days\n\n";
-								msg += "**Subscribers**: " + decimalsInNum(subscribers) + "\n\n";
-								msg += "**Current Mods**: " + decimalsInNum(currentMods) + "\n\n";
-								msg += "**At Least 25 Posts**: " + minimumPosts.toString().capitalise() + "\n\n";
-								if (nsfw) {
-									msg += "**NSFW**: Yes";
-								} else {
-									msg += "**NSFW**: No";
-								}
-								msg += config_bot.credit;
-							}).then(function() { // Submit
-								if (config_bot.interact) {
-									reddit("/api/comment").post({
-										text: msg,
-										thing_id: post_id
-									});
-								}
-								// Report here
-								if (config_bot.reports.should_report) {
-									if (subscribers < config_bot.minimum_subs) {
-										reportReason += config_bot.reports.reason_subs;
-										canReport = true;
-									}
-									if (!minimumPosts) {
-										reportReason += config_bot.reports.reason_posts;
-										canReport = true;
-									}
-									if (!isMod) {
-										reportReason += config_bot.reports.reason_mod;
-										canReport = true;
-									}
-
-									if (canReport) {
-										if (config_bot.interact) {
-											reddit("/api/report").post({
-												reason: reportReason,
-												thing_id: post_id
-											}).then(function() {
-												logPost(post_id, "Reporting for: " + reportReason);
-											});
-										} else {
-											logPost(post_id, "Reporting for: " + reportReason);
-										}
-									}
-								}
-
-								logPost(post_id, "Commenting: " + result.data.display_name);
-								addToChecked({
-									post_id: post_id,
-									author: post.data.author,
-									type: "subreddit",
-									time_checked: Date.now() / 1000,
-									didReport: canReport,
-									subreddit: {
-										subName: subName,
-										age: age,
-										subscribers: subscribers,
-										currentMods: currentMods,
-										minimumPosts: minimumPosts,
-										nsfw: nsfw,
-										isMod: isMod
-									},
-									user: {}
-								});
-							});
-						}, function() {
-							logPost(post_id, "/r/" + subName + " is non-accessible.");
-							addToChecked({
-								post_id: post_id,
-								author: post.data.author,
-								type: "noaccess",
-								time_checked: Date.now() / 1000,
-								didReport: canReport,
-								subreddit: {
-									subName: subName
-								},
-								user: {}
-							});
-							return;
-						});
-					} else { // If it is a user
-						var reAll_user = /\/?[uU]\/[a-zA-Z?_\d]+/g;
-						var allText_user = reAll_user.exec(post.data.title);
-						if (allText_user) {
-							var userName = allText_user[0].replace(/\/?[uU]\//g, "");
-							msg += config_bot.offermod_text;
-							msg += config_bot.credit;
-							if (config_bot.interact) {
-								reddit("/api/comment").post({
-									text: msg,
-									thing_id: post_id
-								});
-							}
-							logPost(post_id, "Commenting: " + allText_user[0]);
-							addToChecked({
-								post_id: post_id,
-								author: post.data.author,
-								type: "user",
-								time_checked: Date.now() / 1000,
-								didReport: canReport,
-								subreddit: {},
-								user: {}
-							});
-						}
+						promises.push(
+							r.getSubreddit(subName).fetch().then((sub) => {
+								var sub_name = sub.display_name;
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+								msg += "Subreddit Info (/r/" + sub.display_name + "):\n\n";
+								msg += "**Age**: " + formatNum()
+							})
+						);
+					} else if (allText_user) { // If the title has a user
+						// Code...
 					}
 				})();
 			}
-		}, function() {
-			logPost("", "/r/" + config_bot.subreddit + " is not accessible.");
-			clearInterval(loop);
+			// Wait for all subreddits to be processed, then end program
+			Promise.all(promises).then(function(values) {
+				db.close();
+				log("Checks finished");
+				process.exit();
+			});
 		});
 	}
 	update();
-	var loop = setInterval(update, config_bot.loop_delay * 1000 * 60);
 }
