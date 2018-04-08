ac9695ace1ad4b298924be70ac568e1d070e0a3c
MatthewWid <game1000@hotmail.co.uk>
2018-04-08 23:49:38 +1000

    Bug fixes and minor additions - Added debug() log function and debug key/value in config_bot.json to differentiate normal logging and debug logging and allowing for a toggle on/off for debug logging. - Added reference code to private message reply credit so that there is a direct link to the message ID for technical support purposes. - Added saving of a users' UTC age in database, instead of just age in days on user posts. - Updated giveFlair() so flair will not be assigned if the interact flag in config_bot.json is set to false. - Updated the flair criteria check to be more optimised in a loop rather than having five if statements. - Updated subreddit message check loop to limit checks to 25 subreddits to check per message, instead of infinite subreddits. - Fixed bug in user profile checking where bot would comment regardless of whether the interact flag in config_bot.json was set to true or false. - Fixed bug where posting a non-existent subreddit or user would crash the bot. - Fixed bug where posts made by deleted accounts crashed the bot. - Fixed bug in user account age checking where the wrong age variable was being checked, resulting in a report never being filed for accounts that are too young. - Removed /r/AutoMobBot subreddit link in comment and message credit.

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ const config_oauth = JSON.parse(fs.readFileSync(__dirname + "/config_oauth.json"
 const config_bot = JSON.parse(fs.readFileSync(__dirname + "/config_bot.json"));
 const config_db = JSON.parse(fs.readFileSync(__dirname + "/config_db.json"));
 config_bot.get_posts = Math.min(config_bot.get_posts, 25);
-config_bot.user_flairs.exp.max = Infinity;
+config_bot.user_flairs.categories.exp.max = Infinity;
 
 const r = new snoowrap(config_oauth);
 
@@ -38,10 +38,17 @@ function log(msg) {
 	console.log("\n", msg);
 }
 
+// Log a debug message if the bot is set to debug mode in the configuration
+function debug(msg) {
+	if (config_bot.debug) {
+		console.log("\n", msg);
+	}
+}
+
 // Connect to MongoDB, collect all previously checked posts, and run the main function
 mongo.connect(config_db.url, function(err, datab) {
 	checkErrorBlank(err);
-	log("Successfully connected to MongoDB server.");
+	debug("Successfully connected to MongoDB server.");
 
 	var col = datab.db("needamod-subreddits").collection(config_db.col);
 	col.find({}).toArray(function(err, docs) {
@@ -101,7 +108,7 @@ function main(db) {
 	// Main function of the bot that is called each loop
 	function update() {
 		var timeNow = new Date();
-		log("Checks started (" + timeNow.toLocaleTimeString("en-US") + ")");
+		debug("Checks started (" + timeNow.toLocaleTimeString("en-US") + ")");
 
 		var mainPromises = [];
 
@@ -113,19 +120,19 @@ function main(db) {
 				for (var i = 0; i < posts.length; i++) {
 					// If the post has already been checked do not check it
 					if (allPosts.find((e) => e.post_id == posts[i].id)) {
-						log(posts[i].id + " | Skipping | Already checked.");
+						debug(posts[i].id + " | Skipping | Already checked.");
 						continue;
 					}
 
 					// If the post is too young to be checked do not check it
 					if ((timeNow - posts[i].created_utc) / 60 < config_bot.wait_time) {
-						log(posts[i].id + " | Skipping | Too young.")
+						debug(posts[i].id + " | Skipping | Too young.")
 						continue;
 					}
 
 					// If the post has a "Mod Post" flair do not check it
 					if (posts[i].link_flair_text == config_bot.post_flair_names.mod) {
-						log(posts[i].id + " | Skipping | Mod post.");
+						debug(posts[i].id + " | Skipping | Mod post.");
 						continue;
 					}
 
@@ -135,6 +142,7 @@ function main(db) {
 			}).then((posts) => {
 				// Push all promises to subreddit into an array, wait for all promises to be processed in that array
 				var promises = [];
+				loop1:
 				for (var l = 0; l < posts.length; l++) {
 					(function() {
 						const i = l;
@@ -165,8 +173,6 @@ function main(db) {
 									};
 
 									msg += "Subreddit Info (/r/" + subInfo.name + "):\n\n";
-									// TODO:
-									// Convert the age to Years, Months, Days in the comment.
 									msg += "**Age**: " + formatAge(subInfo.realAge) + "\n\n";
 									msg += "**Subscribers**: " + formatNum(subInfo.subscribers) + "\n\n";
 
@@ -208,14 +214,12 @@ function main(db) {
 											canReport = true;
 										}
 
-										if (canReport) {
-											if (config_bot.interact) {
-												return posts[i].report(
-													{
-														reason: reportReason
-													}
-												)
-											}
+										if (canReport && config_bot.interact) {
+											return posts[i].report(
+												{
+													reason: reportReason
+												}
+											);
 										}
 									}
 
@@ -223,6 +227,8 @@ function main(db) {
 								}).then(() => {
 									if (config_bot.interact) {
 										log(posts[i].id + " | Commenting | Subreddit");
+									} else {
+										debug(posts[i].id + " | Commenting | Subreddit");
 									}
 									addToChecked({
 									 	post_id: posts[i].id,
@@ -232,13 +238,26 @@ function main(db) {
 									 	didReport: canReport,
 									 	subreddit: {
 									 		subName: subInfo.name,
-									 		age: subInfo.age,
+									 		age: subInfo.ageDays,
 									 		realAge: subInfo.realAge,
 									 		subscribers: subInfo.subscribers,
 									 		currentMods: subInfo.currentMods,
 									 		minimumPosts: subInfo.minimumPosts,
 									 		nsfw: subInfo.nsfw,
 									 		isMod: isMod
+									 	},
+									 	user: {}
+									});
+								}).catch((err) => {
+									log(posts[i].id + " | Error | Getting subreddit: " + subName);
+									addToChecked({
+									 	post_id: posts[i].id,
+									 	author: posts[i].author.name,
+									 	type: "subreddit",
+									 	timeChecked: Date.now() / 1000,
+									 	didReport: canReport,
+									 	subreddit: {
+									 		subName: subName
 									 	},
 									 	user: {}
 									});
@@ -249,9 +268,9 @@ function main(db) {
 							var userInfo;
 
 							promises.push(
-								r.getUser(posts[i].author.name).fetch().then((user) => {
+								r.getUser(userName).fetch().then((user) => {
 									userInfo = {
-										name: posts[i].author.name,
+										name: userName,
 										totalKarma: user.link_karma + user.comment_karma,
 										ageDays: Math.floor((Date.now() / 1000 - user.created_utc) / 60 / 60 / 24),
 										realAge: Math.floor((Date.now() / 1000 - user.created_utc))
@@ -263,26 +282,28 @@ function main(db) {
 									msg += config_bot.offermod_text;
 									msg += config_bot.credit;
 
-									return posts[i].reply(msg);
+									if (config_bot.interact) {
+										return posts[i].reply(msg);
+									} else {
+										return;
+									}
 								}).then(() => {
 									if (config_bot.reports.should_report) {
 										if (userInfo.totalKarma < 500) {
 											reportReason += config_bot.reports.reason_karma;
 											canReport = true;
 										}
-										if (userInfo.age < 90) {
+										if (userInfo.ageDays < 90) {
 											reportReason += config_bot.reports.reason_age;
 											canReport = true;
 										}
 
-										if (canReport) {
-											if (config_bot.interact) {
-												return posts[i].report(
-													{
-														reason: reportReason
-													}
-												)
-											}
+										if (canReport && config_bot.interact) {
+											return posts[i].report(
+												{
+													reason: reportReason
+												}
+											);
 										}
 									}
 
@@ -290,6 +311,8 @@ function main(db) {
 								}).then(() => {
 									if (config_bot.interact) {
 										log(posts[i].id + " | Commenting | User");
+									} else {
+										debug(posts[i].id + " | Commenting | User");
 									}
 									addToChecked({
 									 	post_id: posts[i].id,
@@ -299,8 +322,23 @@ function main(db) {
 									 	didReport: canReport,
 									 	subreddit: {},
 									 	user: {
+									 		name: userInfo.name,
 									 		totalKarma: userInfo.totalKarma,
-									 		age: userInfo.age
+									 		age: userInfo.ageDays,
+									 		realAge: userInfo.realAge
+									 	}
+									});
+								}).catch((err) => {
+									log(posts[i].id + " | Error | Getting user: " + userName);
+									addToChecked({
+									 	post_id: posts[i].id,
+									 	author: "[deleted]",
+									 	type: "subreddit",
+									 	timeChecked: Date.now() / 1000,
+									 	didReport: canReport,
+									 	subreddit: {},
+									 	user: {
+									 		name: userName
 									 	}
 									});
 								})
@@ -310,13 +348,13 @@ function main(db) {
 				}
 				// Wait for all subreddits to be processed, then end program
 				return Promise.all(promises);
+			}).catch((err) => {
+				debug("Overall | Error | Getting base subreddit: " + config_bot.subreddit);
 			})
 		);
 
 		mainPromises.push(
-			// TODO:
-			// If config_bot.interact is set to false, do not do anything in this section.
-			r.getUnreadMessages({filter: "messages"}).then((messages) => {
+			r.getInbox({limit: 1, filter: "messages"}).then((messages) => {
 				var promiseArr = [];
 
 				for (var l = 0; l < messages.length; l++) {
@@ -324,20 +362,17 @@ function main(db) {
 						const i = l;
 
 						if (messages[i].subject.toLowerCase().indexOf("flair") != -1) {
-							var subreddits = messages[i].body.split(" ");
-							// var info = {
-							// 	author: messages[i].author.name,
-							// 	totalSubs: 0
-							// };
+							var lines = messages[i].body.split("\n\n");
+
+							var rawSubs = lines[0].split(" ");
+							var templates = lines[1].split(" ");
+
 							var subs = [];
 							var subNames = [];
 
-							
-							// TODO:
-							// Have a limited amount of subreddits that a user can supply (So they can't give 1000 subreddits and keep the bot waiting for an extended period of time).
-							for (var j = 0; j < subreddits.length; j++) { // Get each subreddit in the message
+							for (var j = 0; j < Math.min(rawSubs.length, 25); j++) { // Get each subreddit in the message
 								(function() {
-									var sub = subreddits[j].replace(/\/?[rR]\//g, "");
+									var sub = rawSubs[j].replace(/\/?[rR]\//g, "");
 
 									subs.push( // Push each promise to the subs array
 										r.getSubreddit(sub).getModerators().then((mods) => { // Get the mod list
@@ -373,7 +408,8 @@ function main(db) {
 									return { // Return the author and the total amount of subscribers they have in their subreddits
 										msg: messages[i],
 										author: messages[i].author,
-										totalSubs: totalSubs
+										totalSubs: totalSubs,
+										templates: templates
 									};
 								})
 							);
@@ -387,63 +423,46 @@ function main(db) {
 				var promiseArr = [];
 
 				function giveFlair(req, text, flair) {
-					if (text != undefined && flair != undefined) { // Give them a flair
-						return req.author.assignFlair({
-							subredditName: config_bot.subreddit,
-							text: text,
-							cssClass: flair.css
-						}).then(() => {
-							return req.msg.reply("**You have been given the " + flair.name + " flair** (" + formatNum(req.totalSubs) + " subscribers total).\n\n" + config_bot.mistake_credit + config_bot.credit);
-						});
-					} else { // If text and CSS have not been supplied, remove their flair
-						return req.author.assignFlair({
-							subredditName: config_bot.subreddit
-						}).then(() => {
-							return req.msg.reply("**Your flair has been removed**.\n\n" + config_bot.mistake_credit + config_bot.credit);
-						});
+					if (config_bot.interact) {
+						log(req.msg.id + " | Flairing | " + (flair != undefined ? flair.name : "No Flair"));
+						if (text != undefined && flair != undefined) { // Give them a flair
+							return req.author.assignFlair({
+								subredditName: config_bot.subreddit,
+								text: text,
+								cssClass: flair.css
+							}).then(() => {
+								return req.msg.reply("**You have been given the " + flair.name + " flair** (" + formatNum(req.totalSubs) + " subscribers total).\n\n" + config_bot.mistake_credit + config_bot.credit + " ^| ^Ref.: ^" + req.msg.id);
+							});
+						} else { // If text and CSS have not been supplied, remove their flair
+							return req.author.assignFlair({
+								subredditName: config_bot.subreddit
+							}).then(() => {
+								return req.msg.reply("**Your flair has been removed**.\n\n" + config_bot.mistake_credit + config_bot.credit + " ^| ^Ref.: ^" + req.msg.id);
+							});
+						}
+					} else {
+						return;
 					}
 				}
 				function fitsCriteria(totalSubs, flair_rules) { // Check if the total amount of subscribers fit between the given criteria
 					return (totalSubs >= flair_rules.min && totalSubs <= flair_rules.max);
 				}
 
+				loop1:
 				for (var i = 0; i < allReturns.length; i++) {
 					const flairReq = allReturns[i];
-					const flairs = config_bot.user_flairs;
-
-					// TODO:
-					// Optimise this mess.
 					
-					if (fitsCriteria(flairReq.totalSubs, flairs.lrn)) { // Check and assign the "Learning" flair
-						promiseArr.push(
-							giveFlair(flairReq, "", flairs.lrn)
-						);
-						continue;
+					var flairNames = Object.getOwnPropertyNames(config_bot.user_flairs.categories);
+					for (var l = 0; l < flairNames.length; l++) {
+						if (fitsCriteria(flairReq.totalSubs, config_bot.user_flairs.categories[flairNames[l]])) {
+							promiseArr.push(
+								giveFlair(flairReq, "", config_bot.user_flairs.categories[flairNames[l]])
+							);
+							continue loop1;
+						}
 					}
-					if (fitsCriteria(flairReq.totalSubs, flairs.nov)) { // Check and assign the "Novice" flair
-						promiseArr.push(
-							giveFlair(flairReq, "", flairs.nov)
-						);
-						continue;
-					}
-					if (fitsCriteria(flairReq.totalSubs, flairs.int)) { // Check and assign the "Intermediate" flair
-						promiseArr.push(
-							giveFlair(flairReq, "", flairs.int)
-						);
-						continue;
-					}
-					if (fitsCriteria(flairReq.totalSubs, flairs.adv)) { // Check and assign the "Advanced" flair
-						promiseArr.push(
-							giveFlair(flairReq, "", flairs.adv)
-						);
-						continue;
-					}
-					if (fitsCriteria(flairReq.totalSubs, flairs.exp)) { // Check and assign the "Expert" flair
-						promiseArr.push(
-							giveFlair(flairReq, "", flairs.exp)
-						);
-						continue;
-					}
+					// TODO:
+					// If a user does not supply any subreddits, keep their current flair and just give them the text they requested
 					promiseArr.push( // If they don't fit the criteria for any flair, remove their current flair
 						giveFlair(flairReq)
 					);
@@ -455,9 +474,9 @@ function main(db) {
 			})
 		);
 
-		Promise.all(mainPromises).then((output) => {
+		Promise.all(mainPromises).then(() => {
 			db.close();
-			log("Checks finished");
+			debug("Checks finished");
 			process.exit();
 		});
 	}
